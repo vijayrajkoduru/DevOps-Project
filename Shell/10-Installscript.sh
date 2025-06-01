109d8dcdb5a7e5d31ba5ab5354d3552c76bb8cdf
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-01 18:32:44 +0530

    created new file

--- Shell/10-Installscript.sh
+++ Shell/10-Installscript.sh
@@ -2,20 +2,50 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then
-    echo "You must run this script as root"
-    exit 1
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
 fi
 
-dnf install mysql -y
+dnf list installed mysql
 
-if [ $? -ne 0 ]; then
-    echo "Failed to install MySQL server"
-    exit 1
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
 fi
 
-dnf install git -y
-if [ $? -ne 0 ]; then
-    echo "Failed to install Git"
-    exit 1
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
+else
+    echo "Git is already ... INSTALLED"
 fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
