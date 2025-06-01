653a9215e92e5fc5c06da5f9bdeef42a4c35d291
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-01 14:13:10 +0530

    hellow world added

---
+++ Shell/02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "vijay :: hi kumar"
+echo "srikanth :: hi Pavani"
+echo "pavani:: How are you doing?"
+echo "Robert :: I am doing great, thanks for asking!"
---
+++ Shell/03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1="vijay"
+PERSON2="kumar"
+
+
+echo "$PERSON1 :: hi $PERSON2"
+echo "$PERSON2 :: hi $PERSON1"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2 :: I am doing great, thanks for asking!"
