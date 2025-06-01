78595a4365393d9ec26f3d6118b59ec4f17147ec
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-01 18:07:55 +0530

    condition file is create

--- Shell/09-conditions.sh
+++ Shell/09-conditions.sh
@@ -2,16 +2,7 @@
 
 NUMBER=$1
 
-# -gt: greater than
-# -eq: equal to
-# -lt: less than
-# -ne: not equal to
-# -ge: greater than or equal to
-# -le: less than or equal to
-
-
-if [$NUMBER -gt 100]; 
-then
+if [ $NUMBER -gt 100 ]; then
     echo "The number is greater than 100"
 else
     echo "The number is less than 100"
