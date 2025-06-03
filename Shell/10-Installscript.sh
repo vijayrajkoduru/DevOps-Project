499d492635e380495d5014c31c39e7a92dfe7f81
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-03 08:37:22 +0530

    created new files

--- Shell/10-Installscript.sh
+++ Shell/10-Installscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USERID=$(id -u)
+USERID=$(id -u) #
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] 
 then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
