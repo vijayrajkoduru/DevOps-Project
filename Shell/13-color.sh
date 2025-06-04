4631c9ddd1bcaa22d9e4614b57a76a5073813e76
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-04 17:22:28 +0530

    modified

--- Shell/13-color.sh
+++ Shell/13-color.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+
 # Check for root access
 USERID=$(id -u) # Get the user ID of the current user
 if [ $USERID -ne 0 ]; then
@@ -11,18 +17,18 @@ fi
 install_package() { # This function installs a package if it is not already installed
     PACKAGE=$1 # Get the package name from the argument
 
-    echo "Checking if $PACKAGE is installed..."
+    echo "Checking if $PACKAGE is $G installed..."
     dnf list installed $PACKAGE # &>/dev/null or can give log
  
     if [ $? -ne 0 ]; then
-        echo "$PACKAGE is not installed. Installing..."
+        echo "$PACKAGE is not installed. $Y Installing..."
         dnf install -y $PACKAGE # &>/dev/null or can give log
 
         if [ $? -ne 0 ]; then
             echo "Installing $PACKAGE ... FAILURE"
             exit 1 # Exit with an error code if installation fails
         else
-            echo "Installing $PACKAGE ... SUCCESS"
+            echo "Installing $PACKAGE ... $R SUCCESS"
         fi
     else
         echo "$PACKAGE is already ... INSTALLED"
