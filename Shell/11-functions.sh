d5f2b9b6c9d599f48d2806ba07cd1bcb6eded390
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-04 16:15:12 +0530

    modifies

---
+++ Shell/11-functions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check for root access
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1
+fi
+
+# Function to install a package
+install_package() {
+    PACKAGE=$1
+
+    echo "Checking if $PACKAGE is installed..."
+    dnf list installed $PACKAGE &>/dev/null
+
+    if [ $? -ne 0 ]; then
+        echo "$PACKAGE is not installed. Installing..."
+        dnf install -y $PACKAGE &>/dev/null
+
+        if [ $? -ne 0 ]; then
+            echo "Installing $PACKAGE ... FAILURE"
+            exit 1
+        else
+            echo "Installing $PACKAGE ... SUCCESS"
+        fi
+    else
+        echo "$PACKAGE is already ... INSTALLED"
+    fi
+}
+
+# Call the function with required packages
+install_package mysql
+install_package git
