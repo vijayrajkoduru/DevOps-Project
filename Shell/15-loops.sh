d037d918c6bda6b138df8b68e500610396f92e2e
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-05 20:26:40 +0530

    creataed some files

---
+++ Shell/15-loops.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in {0..5}
+do
+    echo Iteration:- $i
+done
+
+# echo 1
+# echo 2
+# echo 3
+# echo 4
+# echo 5
---
+++ Shell/16-loops.sh
@@ -0,0 +1,49 @@
+#!/bin/bas
+
+USER-ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+B="\e[34m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG-FOLDER="/home/ec2-user/shell-scripts.log"
+LOG-FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAM=$(date +"%Y-%m-%d %H:%M:%S")
+LOG-FILE_NAME=$LOG-FOLDER/$LOG-FILE.log-$TIMESTAM.lo
+VALIDATE(){
+
+    if[$1 -ne 0]
+    then 
+
+        echo -e "$2.. ${R}FAILED${N}"
+        exit 1
+    else
+        echo -e "$2.. ${G}SUCCESS${N}"
+    fi
+}
+mkdir -p "/home/ec2-user/shell-scripts.log"
+
+ROOT(){
+    
+    if [ $USER-ID -ne 0 ]
+    then
+        echo -e "${R}You must be root to run this script${N}"
+        exit 1
+    fi  
+}
+
+ROOT
+
+for package in $@
+do
+    dnf install $package -y &>> $LOG-FILE_NAME
+    if [$? -ne 0]
+    then
+        dnf
+    else
+        echo -e "${G}Successfully installed $package${N}"
+        VALIDATE $? "Installation of $package"
+    else
+        echo -e "$package is already $Y installed${N}"
+   fi
