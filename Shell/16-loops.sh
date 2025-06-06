be384c5326e64acd52b5d9d50601c8ce88eb4784
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-06 09:08:00 +0530

    deleted and added

--- Shell/16-loops.sh
+++ Shell/16-loops.sh
@@ -1,48 +1,54 @@
-#!/bin/bas
+#!/bin/bash
 
-USER-ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-B="\e[34m"
-Y="\e[33m"
-N="\e[0m"
+# ===== Runtime Variables =====
+USERID=$(id -u)
+R="\e[31m"        # Red
+G="\e[32m"        # Green
+Y="\e[33m"        # Yellow
+N="\e[0m"         # Reset to normal
 
-LOG-FOLDER="/home/ec2-user/shell-scripts.log"
-mkdir -p $LOG-FOLDER
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+mkdir -p "$LOGS_FOLDER"
 
-LOG-FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAM=$(date +%Y-%m-%d %H:%M:%S)
-LOG-FILE_NAME="$LOG-FOLDER/$LOG-FILE.log-$TIMESTAM.log"
+LOG_FILE=$(basename "$0" | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-VALIDATE(){
-    if [$1 -ne 0]
-    then 
-        echo -e "$2.. ${R}FAILED${N}"
+# ===== Function to validate command execution =====
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N"
+        echo "$2 ... FAILURE" >>"$LOG_FILE_NAME"
         exit 1
     else
-        echo -e "$2.. ${G}SUCCESS${N}"
+        echo -e "$2 ... $G SUCCESS $N"
+        echo "$2 ... SUCCESS" >>"$LOG_FILE_NAME"
     fi
 }
 
-ROOT(){
-    if [ $USER-ID -ne 0 ]
-    then
-        echo -e "${R}You must be root to run this script${N}"
+# ===== Function to check root privileges =====
+CHECK_ROOT() {
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R ERROR::$N You must have sudo access to execute this script"
+        echo "ERROR:: Script requires root/sudo access" >>"$LOG_FILE_NAME"
         exit 1
-    fi  
+    fi
 }
 
-echo "script executed at: $TIMESTAM" >> $LOG-FILE_NAME
+# ===== Start Logging =====
+echo "Script started executing at: $TIMESTAMP" | tee -a "$LOG_FILE_NAME"
 
-ROOT
+# ===== Root Privilege Check =====
+CHECK_ROOT
 
-for package in $@
-do
-    dnf install $package -y &>> $LOG-FILE_NAME
-    if [$? -ne 0]
-    then
-        dnf install $package -y &>> $LOG-FILE_NAME
+# ===== Package Installation Loop =====
+for package in "$@"; do
+    dnf list installed "$package" &>>"$LOG_FILE_NAME"
+    if [ $? -ne 0 ]; then
+        dnf install "$package" -y &>>"$LOG_FILE_NAME"
         VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $Y installed${N}"
-   fi
+        echo -e "$package is already $Y INSTALLED $N"
+        echo "$package is already INSTALLED" >>"$LOG_FILE_NAME"
+    fi
+done
